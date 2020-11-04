87976b274c9506527443ef73cfa970367ecddb8c
ferrisbueller66 <tmtarpinian@gmail.com>
2020-11-03 23:38:50 -0500

    write team_colors method

--- hashketball.rb
+++ hashketball.rb
@@ -148,3 +148,12 @@ def shoe_size(name)
     end
   end
 end
+
+
+def team_colors(team_name)
+  game_hash.each do |status, team| 
+      if team[:team_name] == team_name
+        return team[:colors]
+      end
+  end
+end
